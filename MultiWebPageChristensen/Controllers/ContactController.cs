38b38f066528d07711eee742051b492298201a19
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-09-06 11:06:36 -0500

    Added connection string and coded controller

--- MultiWebPageChristensen/Controllers/ContactController.cs
+++ MultiWebPageChristensen/Controllers/ContactController.cs
@@ -1,12 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MultiWebPageChristensen.Models;
 namespace MultiWebPageChristensen.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private ContactContext context;
+
+        public ContactController(ILogger<HomeController> logger, ContactContext dbcontext)
+        {
+            _logger = logger;
+            context = dbcontext;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var contacts = context.Contacts
+                .OrderBy(m => m.Name)
+                .ToList();
+            return View(contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Contact());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var contact = context.Contacts.Find(id);
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                if (contact.ContactId == 0)
+                    context.Contacts.Add(contact);
+                else
+                    context.Contacts.Update(contact);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Movie");
+            }
+            else
+            {
+                ViewBag.Action = (contact.ContactId == 0) ? "Add" : "Edit";
+                return View(contact);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var contact = context.Contacts.Find(id);
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact contact)
+        {
+            context.Contacts.Remove(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Movie");
         }
     }
 }
