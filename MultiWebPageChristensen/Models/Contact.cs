c0479938d1b7025249abcc6f7f79e2c9a123bb66
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-09-06 10:58:36 -0500

    edited models

--- MultiWebPageChristensen/Models/Contact.cs
+++ MultiWebPageChristensen/Models/Contact.cs
@@ -1,6 +1,27 @@
-﻿namespace MultiWebPageChristensen.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security;
+
+namespace MultiWebPageChristensen.Models
 {
     public class Contact
     {
+        public int ContactId { get; set; }
+        [Required(ErrorMessage = "Please enter the Name of the Movie")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the year the Movie came out.")]
+        [Range(1889, 2050, ErrorMessage = "Year must be between 1889 and 2024")]
+        public string Number { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Rating")]
+        [Range(1, 5, ErrorMessage = " Ratings are on the 5 star scale")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Genre")]
+        public string Note { get; set; }
+
+        public string Slug =>
+            Name?.Replace(' ', '-').ToLower() + '-' + Number;
+
     }
 }
--- MultiWebPageChristensen/Models/ContactContext.cs
+++ MultiWebPageChristensen/Models/ContactContext.cs
@@ -1,6 +1,48 @@
-﻿namespace MultiWebPageChristensen.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MultiWebPageChristensen.Models
 {
-    public class ContactContext
+    public class ContactContext : DbContext
     {
+        public ContactContext(DbContextOptions<ContactContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Contact> Contacts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact
+                {
+                    ContactId = 1,
+                    Name = "Morgan",
+                    Number = "(712)-555-6658",
+                    Address = "3520 1st Street",
+                    Note = "Iowa"
+                },
+                new Contact
+                {
+                    ContactId = 2,
+                    Name = "Carlos",
+                    Number = "(515)-265-3356",
+                    Address = "5560 2nd Street",
+                    Note = "Kansas"
+                },
+                new Contact
+                {
+                    ContactId = 3,
+                    Name = "Victoria",
+                    Number = "(659)-889-4213",
+                    Address = "720 2nd Ave",
+                    Note = "Ohio"
+                }
+
+            );
+        }
     }
 }
+
